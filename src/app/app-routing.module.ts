97fbc0bfdc02d7aa792ac50ccea40d2f61da0a1d
VicoVillca <vicovillca@hotmail.com>
2022-09-28 09:37:11 -0400

    agregamos toolbar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DominicodeComponent } from './pages/dominicode/dominicode.component';
+
+const routes: Routes = [
+  {path:'dominicode',component:DominicodeComponent},
+  { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+  {path:'**',redirectTo:'',pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
