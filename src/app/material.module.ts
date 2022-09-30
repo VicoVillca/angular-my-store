137b9e7113762c4c89d5d46988761bacec1f65c8
VicoVillca <vicovillca@hotmail.com>
2022-09-29 23:53:08 -0400

    agregamos los grids de prueba

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,14 +4,15 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 @NgModule ({
   exports:[
     MatSliderModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule
   ]
 })
 export class MaterialModule{}
