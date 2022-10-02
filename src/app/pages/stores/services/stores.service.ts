ec86af8674cdac4fa75c55904ccc027a567d6c24
VicoVillca <vicovillca@hotmail.com>
2022-10-02 09:34:48 -0400

    agegamos modificaciones

--- src/app/pages/stores/services/stores.service.ts
+++ src/app/pages/stores/services/stores.service.ts
@@ -1,8 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Store } from "../interfaces/store.interface";
+
+/*import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Producto } from '../../products/interfaces/product.interface';
 import { Store } from '../interfaces/store.interface';
 
 @Injectable({
@@ -16,5 +21,17 @@ export class StoresService {
     console.log(this.apiUrl);
     return this.http.get<Store[]>(this.apiUrl);
 
+  }
+}*/
+@Injectable({
+  providedIn:'root'
+})
+
+export class StoresService{
+  constructor(private Http:HttpClient){}
+  private apiUrl = environment.apiUrl+"/stores";
+  getStores():Observable<Store[]>{
+    console.log(this.apiUrl);
+    return this.Http.get<Store[]>(this.apiUrl);
   }
 }
